c562a2ebe682c8a0c68199455517d77ac07fcd3e
?ngel ?lvarez <angel@nan-tic.com>
2018-02-01 14:49:06 +0100

    update to 4.7

--- tests/test_project_menu_tree_remove.py
+++ tests/test_project_menu_tree_remove.py
@@ -5,12 +5,13 @@ import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
 
 
-class TestCase(ModuleTestCase):
-    'Test module'
+class ProjectMenuRemoveTestCase(ModuleTestCase):
+    'Test ProjectMenuRemove module'
     module = 'project_menu_tree_remove'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
+        ProjectMenuRemoveTestCase))
     return suite
